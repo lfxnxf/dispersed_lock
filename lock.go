c92ac1214aeb147724d30636485fd58bbc25c998
lfxnxf <313447221@qq.com>
2022-04-06 14:23:40 +0800

    Update lock.go
    增加看门狗

--- lock.go
+++ lock.go
@@ -1,17 +1,27 @@
 package dispersed_lock
 
 import (
-	"github.com/gomodule/redigo/redis"
-	"github.com/lfxnxf/while"
-	"math/rand"
-	"strings"
+	"context"
+	"fmt"
+	"git.huajiao.com/infra/go-kit/zlog"
+	"git.huajiao.com/infra/go-zero/core/stores/redis"
+	"git.huajiao.com/infra/go-zero/core/utils"
+	"go.uber.org/zap"
 	"sync"
 	"time"
 )
 
 const (
-	lockScript = "if redis.call('get', KEYS[1]) == ARGV[1] " +
+	// 解锁lua
+	unLockScript = "if redis.call('get', KEYS[1]) == ARGV[1] " +
 		"then redis.call('del', KEYS[1]) return 1 " +
+		"else " +
+		"return 0 " +
+		"end"
+
+	// 看门狗lua
+	watchLogScript = "if redis.call('get', KEYS[1]) == ARGV[1] " +
+		"then return redis.call('expire', KEYS[1], ARGV[2]) " +
 		"else " +
 		"return 0 " +
 		"end"
@@ -24,31 +34,40 @@ var scriptMap sync.Map
 type option func() (bool, error)
 
 type DispersedLock struct {
-	key          string     `json:"key"`           // 锁key
-	value        int64      `json:"value"`         // 锁的值，随机数
-	expire       int64      `json:"expire"`        // 锁过期时间
-	lockClient   redis.Conn `json:"lock_client"`   // 锁客户端，暂时只有redis
-	scriptString string     `json:"script_string"` // lua脚本
-	options      []option   `json:"options"`       // 事件
+	key            string        // 锁key
+	value          string        // 锁的值，随机数
+	expire         int           // 锁过期时间,单位秒
+	lockClient     *redis.Redis  // 锁客户端，暂时只有redis
+	unLockScript   string        // lua脚本
+	watchLogScript string        // 看门狗lua
+	options        []option      // 事件
+	unlockCh       chan struct{} // 解锁通知通道
 }
 
-func New(client redis.Conn, key string, expire int64) *DispersedLock {
+func New(ctx context.Context, client *redis.Redis, key string, expire int) *DispersedLock {
 	d := &DispersedLock{
-		key:        key,
-		expire:     expire,
-		value:      random(100000000, 999999999), // 随机值作为锁的值
-		lockClient: client,
+		key:    key,
+		expire: expire,
+		value:  fmt.Sprintf("%d", utils.Random(100000000, 999999999)), // 随机值作为锁的值
 	}
 
+	//初始化连接
+	d.lockClient = client
+
 	//初始化lua script
-	scriptString, _ := scriptMap.LoadOrStore(key, d.getScript())
-	d.scriptString = scriptString.(string)
+	lockScript, _ := scriptMap.LoadOrStore("dispersed_lock", d.getScript(ctx, unLockScript))
+	watchLogScript, _ := scriptMap.LoadOrStore("watch_log", d.getScript(ctx, watchLogScript))
+
+	d.unLockScript = lockScript.(string)
+	d.watchLogScript = watchLogScript.(string)
+
+	d.unlockCh = make(chan struct{}, 0)
 
 	return d
 }
 
-func (d *DispersedLock) getScript() string {
-	scriptString, _ := redis.String(d.lockClient.Do("SCRIPT", "LOAD", lockScript))
+func (d *DispersedLock) getScript(ctx context.Context, script string) string {
+	scriptString, _ := d.lockClient.ScriptLoad(ctx, script)
 	return scriptString
 }
 
@@ -58,20 +77,12 @@ func (d *DispersedLock) RegisterOptions(f ...option) {
 }
 
 //加锁
-func (d *DispersedLock) Lock() bool {
-	args := []interface{}{
-		d.key,    // 锁的key
-		d.value,  // 锁的随机值
-		"ex",     // 设置过期
-		d.expire, // 过期时间
-		"nx",     // 设置不可重复设置
+func (d *DispersedLock) Lock(ctx context.Context) bool {
+	ok, _ := d.lockClient.SetnxEx(ctx, d.key, d.value, d.expire)
+	if ok {
+		go d.watchDog(ctx)
 	}
-	ret, _ := redis.String(d.lockClient.Do("Set", args...))
-	if strings.ToLower(ret) == "ok" {
-		return true
-	} else {
-		return false
-	}
+	return ok
 }
 
 //循环加锁
@@ -94,12 +105,12 @@ func (d *DispersedLock) LoopLock(ctx context.Context, sleepTime int) bool {
 
 //循环获取锁并且绑定事件
 //eg:单个线程获取缓存、其它线程等待
-func (d *DispersedLock) LoopLockWithOption(sleepTime int) (bool, error) {
+func (d *DispersedLock) LoopLockWithOption(ctx context.Context, sleepTime int) (bool, error) {
 	t := time.NewTicker(time.Duration(sleepTime) * time.Millisecond)
-	w := while.NewWhile(lockMaxLoopNum)
+	w := utils.NewWhile(lockMaxLoopNum)
 	var err error
 	w.For(func() {
-		locked := d.Lock()
+		locked := d.Lock(ctx)
 		if locked { // 获取到锁，跳出循环
 			t.Stop()
 			w.Break()
@@ -129,15 +140,45 @@ func (d *DispersedLock) LoopLockWithOption(sleepTime int) (bool, error) {
 }
 
 //解锁
-func (d *DispersedLock) Unlock() bool {
+func (d *DispersedLock) Unlock(ctx context.Context) bool {
 	args := []interface{}{
-		d.scriptString, // 脚本sha1值
-		1,              // key的length
-		d.key,          // key
-		d.value,        // 脚本中的arg
+		d.value, // 脚本中的argv
 	}
-	flag, _ := redis.Int64(d.lockClient.Do("EvalSha", args...))
-	return lockRes(flag)
+	flag, _ := d.lockClient.EvalSha(ctx, d.unLockScript, []string{d.key}, args...)
+	// 关闭看门狗
+	d.unlockCh <- struct{}{}
+	return lockRes(flag.(int64))
+}
+
+//看门狗
+func (d *DispersedLock) watchDog(ctx context.Context) {
+	// 创建一个定时器NewTicker, 每过期时间的3分之2触发一次
+	loopTime := time.Duration(d.expire*1e3/3) * time.Millisecond
+	expTicker := time.NewTicker(loopTime)
+	//确认锁与锁续期打包原子化
+	for {
+		select {
+		case <-expTicker.C:
+			args := []interface{}{
+				d.value,
+				d.expire,
+			}
+			res, err := d.lockClient.EvalSha(ctx, d.watchLogScript, []string{d.key}, args...)
+			if err != nil {
+				zlog.TraceError(ctx, "watchDog error", zap.Error(err))
+				return
+			}
+			r, ok := res.(int64)
+			if !ok {
+				return
+			}
+			if r == 0 {
+				return
+			}
+		case <-d.unlockCh: //任务完成后用户解锁通知看门狗退出
+			return
+		}
+	}
 }
 
 func lockRes(flag int64) bool {
@@ -148,7 +189,3 @@ func lockRes(flag int64) bool {
 	}
 }
 
-func random(min, max int64) int64 {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Int63n(max-min+1) + min
-}
